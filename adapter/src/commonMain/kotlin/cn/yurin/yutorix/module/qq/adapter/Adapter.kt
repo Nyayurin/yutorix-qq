7f11e4072acf52245f8a511551b9466fd9b4c41d
Nyayurin <nyayurn@outlook.com>
2025-02-09 14:14:51 +0800

    更新 yutori adapter 移除 ktor 引擎 格式化代码

--- adapter/src/commonMain/kotlin/cn/yurin/yutorix/module/qq/adapter/Adapter.kt
+++ adapter/src/commonMain/kotlin/cn/yurin/yutorix/module/qq/adapter/Adapter.kt
@@ -2,74 +2,33 @@
 
 package cn.yurin.yutorix.module.qq.adapter
 
-import cn.yurin.yutori.*
-import cn.yurin.yutorix.module.qq.SatoriAdapterProperties
-import co.touchlab.kermit.Logger
-import kotlinx.atomicfu.atomic
-import kotlinx.coroutines.delay
+import cn.yurin.yutori.Adapter
+import cn.yurin.yutori.Reinstallable
+import cn.yurin.yutori.Yutori
+import cn.yurin.yutori.YutoriBuilder
 
-fun Adapter.Companion.satori(
-    alias: String? = null,
-    host: String = "127.0.0.1",
-    port: Int = 8080,
-    path: String = "",
-    token: String? = null,
-    version: String = "v1",
-    onStart: suspend WebSocketEventService.() -> Unit = { },
-    onConnect: suspend WebSocketEventService.(List<Login>) -> Unit = { },
-    onClose: suspend () -> Unit = { },
-    onError: suspend () -> Unit = { },
-) = SatoriAdapter(alias, host, port, path, token, version, onStart, onConnect, onClose, onError)
+fun Adapter.Companion.qq(
+	alias: String? = null,
+	id: Long,
+	token: String,
+	secret: String
+) = QQAdapter(alias, id, token, secret)
 
-class SatoriAdapter(
-    alias: String?,
-    val host: String,
-    val port: Int,
-    val path: String,
-    val token: String?,
-    val version: String,
-    val onStart: suspend WebSocketEventService.() -> Unit,
-    val onConnect: suspend WebSocketEventService.(List<Login>) -> Unit,
-    val onClose: suspend () -> Unit,
-    val onError: suspend () -> Unit,
-) : Adapter(alias),
-    Reinstallable {
-    private var connecting by atomic(false)
-    private var service: WebSocketEventService? by atomic(null)
+class QQAdapter(
+	alias: String?,
+	val id: Long,
+	val token: String,
+	val secret: String,
+) : Adapter(alias), Reinstallable {
+	override fun install(builder: YutoriBuilder) {}
 
-    override fun install(builder: YutoriBuilder) {}
+	override fun uninstall(builder: YutoriBuilder) {}
 
-    override fun uninstall(builder: YutoriBuilder) {}
+	override suspend fun start(yutori: Yutori) {
+		TODO()
+	}
 
-    override suspend fun start(yutori: Yutori) {
-        val properties = SatoriAdapterProperties(host, port, path, token, version)
-        connecting = true
-        var sequence: Number? = null
-        do {
-            service =
-                WebSocketEventService(
-                    alias,
-                    properties,
-                    onConnect,
-                    onClose,
-                    onError,
-                    yutori,
-                    sequence,
-                )
-            service!!.onStart()
-            service!!.connect()
-            if (connecting) {
-                sequence = (service as WebSocketEventService).sequence
-                Logger.i(yutori.name) { "将在5秒后尝试重新连接" }
-                delay(5000)
-                Logger.i(yutori.name) { "尝试重新连接" }
-            }
-        } while (connecting)
-    }
-
-    override fun stop(yutori: Yutori) {
-        connecting = false
-        service?.disconnect()
-        service = null
-    }
+	override fun stop(yutori: Yutori) {
+		TODO()
+	}
 }
